2602b010efcf3e87fd601f09f62fa44463aa44a9
Michael <michael.totarella@mail.concordia.ca>
2022-01-30 16:47:21 -0500

    Removed id column from Products

--- src/models.py
+++ src/models.py
@@ -8,7 +8,9 @@ from . import db
 
 class Users(db.Model, UserMixin):
     __tablename__ = "Users"
-    username = db.Column("username", db.String, primary_key=True)
+    # used only for user_loader
+    id = db.Column("id", db.Integer, primary_key=True)
+    username = db.Column("username", db.String, unique=True)
     password = db.Column("password", db.String)
     last_login = db.Column("last_login", db.DateTime(
         timezone=True), default=func.now())
@@ -26,9 +28,9 @@ class Products(db.Model):
     site_name = db.Column("site_name", db.String)
     product_name = db.Column("product_name", db.String)
 
-    def __init__(self, url, site_name, product_name, user_id):
+    def __init__(self, url, site_name, product_name, username):
+        self.username = username
         self.url = url
         self.site_name = site_name
         self.product_name = product_name
-        self.user_id = user_id
         # self.nickname = nickname # currently not implemented
--- src/views.py
+++ src/views.py
@@ -12,7 +12,7 @@ views = Blueprint("views", __name__)
 @views.route("/stock")
 @login_required
 def check_stock():
-    return render_template("stock.html", values=Products.query.filter_by(user_id=current_user.id).all(), check_stock=checkstock)
+    return render_template("stock.html", values=Products.query.filter_by(username=current_user.username).all(), check_stock=checkstock)
 
 
 @views.route("/products", methods=["POST", "GET"])
@@ -23,15 +23,17 @@ def products():
             url = request.form["add-url"]
             if is_url_valid(url):
                 # if link is not already in the database then add it
-                if not Products.query.filter_by(url=url, user_id=current_user.id).first():
+                if not Products.query.filter_by(url=url, username=current_user.username).first():
                     site, name = create_product(url)
-                    db.session.add(Products(url, site, name, current_user.id))
+                    db.session.add(
+                        Products(url, site, name, current_user.username))
                     db.session.commit()
             else:
                 flash("URL not valid.")
         elif "btn-delete" in request.form:
             url = request.form['btn-delete']
-            Products.query.filter_by(url=url, user_id=current_user.id).delete()
+            Products.query.filter_by(
+                url=url, user=current_user.username).delete()
             db.session.commit()
 
-    return render_template("products.html", values=Products.query.filter_by(user_id=current_user.id).all())
+    return render_template("products.html", values=Products.query.filter_by(username=current_user.username).all())
